75cc88fed24a510e010f7baf464170be208529b6
Benjamin Meier-Andersen <ba@marine-travel.dk>
2024-12-12 07:37:49 +0100

    Cleaned code

--- src/code/2024/11/code/2.js
+++ src/code/2024/11/code/2.js
@@ -1,44 +1,35 @@
 export const run = (input) => {
+  let stones = input.map((x) => ({ val: x, amount: 1 }));
 
-  let stones = input.map(x => ({val: x, amount: 1})) 
-
-  for(let run = 0; run < 75; run++) {
+  for (let run = 0; run < 75; run++) {
     let newStones = [];
-    for(let i = 0; i < stones.length; i++) {
+    for (let i = 0; i < stones.length; i++) {
       let curr = stones[i];
-      if(curr.val === 0) {
-        const obj = {val: 1, amount: curr.amount}
-        matchAndAdd(obj, newStones);        
-      }
-      else if(`${curr.val}`.length % 2 === 0) {
+      if (curr.val === 0) {
+        matchAndAdd({ val: 1, amount: curr.amount }, newStones);
+      } else if (`${curr.val}`.length % 2 === 0) {
         const middle = `${curr.val}`.length / 2;
         const first = parseInt(`${curr.val}`.slice(0, middle));
         const second = parseInt(`${curr.val}`.slice(middle));
-        const obj1 = {val: first, amount: curr.amount};
-        const obj2 = {val: second, amount: curr.amount}
-        matchAndAdd(obj1, newStones);  
-        matchAndAdd(obj2, newStones);  
-      }
-      else {
-        const obj = {val: stones[i].val * 2024, amount: curr.amount};
-        matchAndAdd(obj, newStones);        
+        matchAndAdd({ val: first, amount: curr.amount }, newStones);
+        matchAndAdd({ val: second, amount: curr.amount }, newStones);
+      } else {
+        matchAndAdd({ val: stones[i].val * 2024, amount: curr.amount }, newStones);
       }
     }
     stones = newStones;
   }
 
   let res = 0;
-  stones.forEach(stone => {
+  stones.forEach((stone) => {
     res += stone.amount;
-  })
+  });
 
   return res;
 };
 
 const matchAndAdd = (stone, stones) => {
-  const match = stones.find(x => x.val === stone.val);
-  if(match) 
-    match.amount += stone.amount
-  else 
-    stones.push(stone);
-}
+  const match = stones.find((x) => x.val === stone.val);
+  if (match) match.amount += stone.amount;
+  else stones.push(stone);
+};
